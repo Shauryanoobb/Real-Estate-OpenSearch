8c4b133dfe56d9fc5e8b7754e028ad76c357ffd7
Shaurya Khetarpal <shauryakhetarpal@Shauryas-MacBook-Air.local>
2025-09-13 16:09:27 +0530

    folder structure fixed

--- core/opensearch_client.py
+++
@@ -1,8 +0,0 @@
-from opensearchpy import OpenSearch
-
-client = OpenSearch(
-    hosts=[{"host": "localhost", "port": 9200}],
-    http_auth=("admin", "Atharvanoob@1"),  # replace with your creds
-    use_ssl=True,
-    verify_certs=False
-)
